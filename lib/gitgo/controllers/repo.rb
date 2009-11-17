72ca27a946af87220e74ff691af12dc880013749
Simon Chiang <simon.chiang@pinnacol.com>
2009-11-17 08:37:42 -0700

    cleanup of Repo controller

--- lib/gitgo/controllers/repo.rb
+++ lib/gitgo/controllers/repo.rb
@@ -6,38 +6,21 @@ module Gitgo
       set :resource_name, "repo"
       set :views, "views/repo"
       
-      get("/") do 
+      get("/")          { index }
+      get("/idx")       { idx }
+      get("/idx/:key")  {|key| idx(key) }
+      get("/idx/:k/:v") {|key, value| idx(key, value) }
+      get("/status")    { status }
+      get("/fsck")      { fsck }
+      post("/commit")   { commit }
+      post("/reindex")  { reindex }
+      post("/reset")    { reset }
+      
+      def index
         erb :index, :locals => {
           :stats => repo.stats,
           :keys => repo.list
         }
-      end
-      
-      get("/idx")             { idx }
-      get("/idx/:key")        {|key| idx(key) }
-      get("/idx/:key/:value") {|key, value| idx(key, value) }
-      
-      get("/status") do
-        erb :status, :locals => {:status => repo.status}
-      end
-      
-      get("/fsck") do
-        erb :fsck, :locals => {:issues => repo.fsck}
-      end
-      
-      post("/commit") do
-        repo.commit request['message']
-        redirect url("status")
-      end
-      
-      post("/reindex") do
-        repo.reindex! set?('full')
-        redirect url
-      end
-      
-      post("/reset") do
-        repo.reset
-        redirect url("status")
       end
       
       def idx(key=nil, value=nil)
@@ -48,6 +31,29 @@ module Gitgo
           :values => key ? repo.list(key) : [],
           :shas => key && value ? repo.index(key, value) : []
         }
+      end
+      
+      def status
+        erb :status, :locals => {:status => repo.status}
+      end
+      
+      def fsck
+        erb :fsck, :locals => {:issues => repo.fsck}
+      end
+      
+      def commit
+        repo.commit request['message']
+        redirect url("status")
+      end
+      
+      def reindex
+        repo.reindex! set?('full')
+        redirect url
+      end
+      
+      def reset
+        repo.reset
+        redirect url("status")
       end
     end
   end
