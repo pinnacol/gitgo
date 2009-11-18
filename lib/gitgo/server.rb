2204f4af2ddf7594d12305a2b8ecb0939542b758
Simon Chiang <simon.a.chiang@gmail.com>
2009-11-18 16:46:06 -0700

    changed localhost to 127.0.0.1 in executable to play nice with thin, updates for gem

--- lib/gitgo/server.rb
+++ lib/gitgo/server.rb
@@ -6,7 +6,7 @@ require 'gitgo/controllers/wiki'
 
 module Gitgo
   class Server < Controller
-    set :views, "views/server"
+    set :views, File.expand_path("views/server", ROOT)
     
     set :static, true
     get('/')         { timeline }
