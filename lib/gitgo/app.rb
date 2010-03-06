f6cc9435554b5f16c64765d72dcf07d59597ed00
Simon Chiang <simon.chiang@pinnacol.com>
2010-03-05 17:41:04 -0700

    added welcome screen

--- lib/gitgo/app.rb
+++ lib/gitgo/app.rb
@@ -9,27 +9,51 @@ module Gitgo
     set :views, File.expand_path("views/app", ROOT)
     set :static, true
     
-    get('/')         { timeline }
+    get('/')         { repo.head.nil? ? welcome : timeline }
     get('/timeline') { timeline }
+    get('/welcome')  { welcome }
+    
+    post('/setup')   { setup }
     
     use Controllers::Code
     use Controllers::Issue
     use Controllers::Wiki
     use Controllers::Repo
     
+    def welcome
+      erb :welcome, :locals => {
+        :path => git.path,
+        :branch => git.branch
+      }
+    end
+    
+    def setup
+      unless repo.head.nil?
+        raise "#{git.branch} branch already exists"
+      end
+        
+      remote = request['remote']
+      
+      unless remote.empty?
+        git.track(remote)
+        git.pull(remote)
+        Document.update_idx
+      end
+      
+      redirect url('')
+    end
+    
     def timeline
       page = (request[:page] || 0).to_i
       per_page = (request[:per_page] || 5).to_i
       
       author = request[:author].to_s
-      timeline = repo.head.nil? ? nil : begin
-        repo.timeline(:n => per_page, :offset => page * per_page) do |sha|
-          author.empty? || repo[sha]['author'].include?("<#{author}>")
-        end.collect do |sha|
-          Document.cast(repo[sha], sha)
-        end.sort_by do |doc|
-          doc.date
-        end
+      timeline = repo.timeline(:n => per_page, :offset => page * per_page) do |sha|
+        author.empty? || repo[sha]['author'].include?("<#{author}>")
+      end.collect do |sha|
+        Document.cast(repo[sha], sha)
+      end.sort_by do |doc|
+        doc.date
       end
       
       erb :timeline, :locals => {
