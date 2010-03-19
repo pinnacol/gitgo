1eb7e666cebe72a761fb64017469ee4e3b8dda2f
Simon Chiang <simon.chiang@pinnacol.com>
2010-03-19 17:12:54 -0600

    updated jquery/jspec and began work on gitgo.js

--- public/javascript/gitgo.js
+++ public/javascript/gitgo.js
@@ -1,2 +1,30 @@
-Gitgo = {
+var Gitgo = {};
+
+Gitgo.Graph = {
+  attrs: function (element) {
+    var result = [], data = element.attr('graph').split(',', 3);
+    
+    var convert = function (string) {
+      switch(string.length) {
+        case 0:
+          return null;
+        case 1:
+          return [parseInt(string)];
+        default: {
+          var i, ints = [], chars = string.split('');
+          for (i = 0; i < chars.length; i += 1) {
+             ints[i] = parseInt(chars[i]);
+          }
+          return ints;
+        }
+      }
+    };
+    
+    
+    result[0] = parseInt(data[0]);
+    result[1] = convert(data[1]);
+    result[2] = convert(data[2]);
+    
+    return result
+  }
 };
--- public/spec/gitgo_spec.js
+++ public/spec/gitgo_spec.js
@@ -1,8 +1,13 @@
-
-describe 'Gitgo'
-  // describe '.git_url()'
-  //   it 'should create a git url using the project, and location'
-  //     x.should.be y
-  //   end
-  // end
+describe 'Gitgo.Graph'
+  describe '.attrs'
+    it 'should return the graph attributes for the node'
+      simple = $(fixture('simple'))
+      Gitgo.Graph.attrs(simple.find('li:first')).should.eql [0, [0], null]
+    end
+    
+    it 'should correctly parse branch and children indicies'
+      simple = $(fixture('open_tails'))
+      Gitgo.Graph.attrs(simple.find('#1')).should.eql [0, [0,1,2], [1,2]]
+    end
+  end
 end
